1fea09fe42b8ba8da707bc7520e81d9287ada26c
zzz-y <zhangyue0-0@qq.com>
2020-09-17 11:33:56 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<index msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import index from "./components/index.vue";
+
+export default {
+    name: "App",
+    components: {
+        index,
+    },
+};
+</script>
