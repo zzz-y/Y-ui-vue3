93ba02761626f7a98235be12217bbc3346bc57c9
zzz-y <zhangyue0-0@qq.com>
2020-09-29 11:47:16 +0800

    完成基本框架

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 
 <script lang="ts">
 import { provide, ref } from "vue";
+import { router } from "./router";
+
 export default {
   name: "App",
   components: {},
@@ -11,6 +13,12 @@ export default {
     const width = document.documentElement.clientWidth;
     const asideVisible = ref(width <= 500 ? false : true);
     provide("asideVisible", asideVisible);
+    router.afterEach(() => {
+      const width = document.documentElement.clientWidth;
+      if (width <= 500) {
+        asideVisible.value = false;
+      }
+    });
   },
 };
 </script>
